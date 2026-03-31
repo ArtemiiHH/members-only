01a56456a5b4141ee3677018a03a9492355a3dff
ArtemiiHH <artemiibarashkov2003@gmail.com>
2026-03-31 19:33:05 +0300

    Add express static line in app

--- app.js
+++ app.js
@@ -12,6 +12,9 @@ const postRouter = require("./routes/postRouter");
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
+// Read static files
+app.use(express.static("public"));
+
 // Routes
 app.use("/", indexRouter);
 app.use("/", authRouter);
