eced928cc49fb2eb6904437e3cb54423b940ba5c
ArtemiiHH <artemiibarashkov2003@gmail.com>
2026-04-01 15:08:20 +0300

    Redirect back home after sign up + add post page

--- routes/authRouter.js
+++ routes/authRouter.js
@@ -4,8 +4,14 @@ const auth = Router();
 auth.get("/sign-up", (req, res) => {
   res.render("sign-up-form");
 });
+auth.post("/sign-up", (req, res) => {
+  res.redirect("/");
+});
 auth.get("/log-in", (req, res) => {
   res.render("log-in-form");
+});
+auth.post("/log-in", (req, res) => {
+  res.redirect("/posts");
 });
 
 module.exports = auth;
